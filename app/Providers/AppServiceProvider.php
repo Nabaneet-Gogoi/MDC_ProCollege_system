2bbfe51eb11adf048f877c5e167b9886661dc01d
Nabaneet-Gogoi <nabaneetgogoi@gmail.com>
2025-03-11 13:52:33 +0530

    Initial commit: MDC ProCollege System

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        // Set a default string length for database migrations
+        // This fixes the "Specified key was too long" error in MySQL
+        Schema::defaultStringLength(191);
+    }
+}
